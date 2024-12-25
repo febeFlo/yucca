cc66774a00443f000dec5ff19a20f90f911f2c8b
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2024-12-25 11:02:17 +0700

    Fitur Chatbot

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import { MantineProvider } from '@mantine/core'
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <MantineProvider 
+//       withGlobalStyles 
+//       withNormalizeCSS 
+//       theme={{
+//         globalStyles: (_theme) => ({
+//           body: {
+//             width: "100vw",
+//             height: "100vh",
+//           },
+//           "#root": {
+//             width: "100%",
+//             height: "100%",
+//           },
+//         }),
+//       }}
+//     >
+//       <App />
+//     </MantineProvider>
+//   </React.StrictMode>,
+// );
+
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core';
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import './index.css';
+import { CharacterAnimationsProvider } from "./contexts/CharacterAnimations"
+
+createRoot(document.getElementById("root")).render(
+    <MantineProvider>
+    <CharacterAnimationsProvider>
+        <App />
+    </CharacterAnimationsProvider>
+    </MantineProvider>
+)
