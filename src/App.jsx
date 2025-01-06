598e901305211670cb777097355c70c93b3dec3f
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2025-01-07 02:18:49 +0700

    fixing responsive and add some feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,38 @@ import Experience from "./components/Experience";
 import Interface from "./components/Interface";
 import { MantineProvider } from "@mantine/core";
 import Chatbot from "./components/Chatbot";
-
+import React, { useState, useEffect } from "react";
+import logo from "./assets/icons/yucca.png"; 
 function App() {
-  return (
-    <div className="flex h-screen items-center">
-      {/* Left Section: 3D Model */}
-      <div className=" bg-gray-100 h-screen  w-1/2">
-        <MantineProvider>
-        <Interface />
-          <Canvas camera={{ position: [3, 1, 0], fov: 50 }} shadows>
-            <Experience />
-          </Canvas>
-          
-        </MantineProvider>
-      </div>
+  const [isLoading, setIsLoading] = useState(true);
 
-      {/* Right Section: Chatbot */}
-      <div className="w-1/2 bg-white p-4 overflow-auto">
-        <Chatbot />
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-orange-50">
+        <div className="text-center">
+          <img src={logo} alt="Logo" className=" animate-spin mx-auto h-16 w-16 mb-4" />
+          <p className="mt-4 text-orange-600 font-bold">Loading...</p>
+        </div>
       </div>
+    );
+  }
+
+  return (
+    <div className="bg-orange-50 flex flex-col md:flex-row h-screen">
+      <MantineProvider>
+        <Canvas camera={{ position: [3, 1, 0], fov: 50 }} shadows>
+          <Experience />
+        </Canvas>
+      </MantineProvider>
+      <Chatbot />
     </div>
   );
 }
