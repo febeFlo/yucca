493f5bda641b79a6a4b1a774741414c8609ab89b
febeFlo <steffanyflorence@gmail.com>
2025-01-15 21:59:45 +0700

    All animation in

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,37 +1,66 @@
 import { OrbitControls } from "@react-three/drei";
-import Yucca from "./Yucca";
-import YuccaEnterance from "./YuccaEnterance";
-import YuccaMendengar from "./YuccaMendengar";
-import YuccaBerpikir from "./YuccaBerpikir";
-import YuccaBerbicara from "./YuccaBerbicara";
-import Yucca_idleblend5 from "./Yucca_idleblend5";
+import { useCharacterAnimations } from "../contexts/CharacterAnimations";
+import YuccaBerpikirR from "./YuccaBerpikirR";
+import YuccaEntrance from "./YuccaEntrance";
+import YuccaExit from "./YuccaExit";
+import YuccaIdleBiasa from "./YuccaIdleBiasa";
 import YuccaIdleLompat from "./YuccaIdleLompat";
+import YuccaMauMenjawab from "./YuccaMauMenjawab";
+import YuccaMendengarR from "./YuccaMendengarR";
+import YuccaMikir1 from "./YuccaMikir1";
+import YuccaMikir2 from "./YuccaMikir2";
+import YuccaNgantuk from "./YuccaNgantuk";
+import YuccaSedih from "./YuccaSedih";
+import YuccaSenang from "./YuccaSenang";
+import YuccaTungguSebentar_ver1 from "./YuccaTungguSebentar_ver1";
+import YuccaTungguSebentar_ver2 from "./YuccaTungguSebentar_ver2";
+
+const animationsMap = {
+    0: YuccaEntrance,
+    1: YuccaBerpikirR,
+    2: YuccaExit,
+    3: YuccaIdleBiasa,
+    4: YuccaIdleLompat,
+    5: YuccaMauMenjawab,
+    6: YuccaMendengarR,
+    7: YuccaMikir1,
+    8: YuccaMikir2,
+    9: YuccaNgantuk,
+    10: YuccaSedih,
+    11: YuccaSenang,
+    12: YuccaTungguSebentar_ver1,
+    13: YuccaTungguSebentar_ver2,
+};
 
 const Experience = () => {
+
+    const { animationIndex } = useCharacterAnimations();
+    const AnimationComponent = animationsMap[animationIndex];
+
     return (
         <>
-        <OrbitControls />
-        <ambientLight />
-        <directionalLight 
-            position={[-5, 5, 5]} 
-            castShadow 
-            shadow-mapSize-width={1024}
-            shadow-mapSize-height={1024}
-        />
-        <group position={[0, 0, 0]}>
-            <group position={[-2, -1.5, 0]} rotation={[0, Math.PI / 2, 0]}> 
-                {/* Ini yang diganti */}
-                <YuccaIdleLompat /> 
+            <OrbitControls />
+            <ambientLight />
+            <directionalLight
+                position={[-5, 5, 5]}
+                castShadow
+                shadow-mapSize-width={1024}
+                shadow-mapSize-height={1024}
+            />
+            <group position={[0, 0, 0]}>
+                <group position={[-2, -1.5, 0]} rotation={[0, Math.PI / 2, 0]}>
+                    {/* Ini yang diganti */}
+                    {AnimationComponent && <AnimationComponent />}
+                </group>
             </group>
-        </group>
-        <mesh 
-            rotation={[-0.5 * Math.PI, 0, 0]} 
-            position={[0, 0, 0]} 
-            receiveShadow
-        >
-            <planeGeometry args={[10, 10, 1, 1]} />
-            <shadowMaterial transparent opacity={0.2} />
-        </mesh>
+            <mesh
+                rotation={[-0.5 * Math.PI, 0, 0]}
+                position={[0, 0, 0]}
+                receiveShadow
+            >
+                <planeGeometry args={[10, 10, 1, 1]} />
+                <shadowMaterial transparent opacity={0.2} />
+            </mesh>
         </>
     );
 };
--- src/contexts/CharacterAnimations.jsx
+++ src/contexts/CharacterAnimations.jsx
@@ -6,20 +6,22 @@ export const CharacterAnimationsProvider = (props) => {
   const [animationIndex, setAnimationIndex] = useState(0); 
   const [animations, setAnimations] = useState([]);
   const [isSpeaking, setIsSpeaking] = useState(false);
+  const [isListening, setIsListening] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   
-  const idleAnimations = [0];
-  
+  const idleAnimations = [3, 4, 9];
+
   const startRandomAnimations = useCallback(() => {
-    if (!isSpeaking) {
+    if (!isSpeaking && !isListening && !isLoading) {
       const randomIndex = idleAnimations[Math.floor(Math.random() * idleAnimations.length)];
       setAnimationIndex(randomIndex);
     }
-  }, [isSpeaking]);
+  }, [isSpeaking, isListening, isLoading]);
 
   useEffect(() => {
     let intervalId;
     
-    if (!isSpeaking) {
+    if (!isSpeaking && !isListening && !isLoading) {
       startRandomAnimations();
       
       intervalId = setInterval(() => {
@@ -32,14 +34,25 @@ export const CharacterAnimationsProvider = (props) => {
         clearInterval(intervalId);
       }
     };
-  }, [isSpeaking, startRandomAnimations]);
-
+  }, [isSpeaking, isListening, isLoading, startRandomAnimations]);
 
   useEffect(() => {
     if (isSpeaking) {
-      setAnimationIndex(1); 
+      setAnimationIndex(11);
     }
   }, [isSpeaking]);
+
+  useEffect(() => {
+    if (isListening) {
+      setAnimationIndex(6);
+    }
+  }, [isListening]);
+
+  useEffect(() => {
+    if (isLoading) {
+      setAnimationIndex(7);
+    }
+  }, [isLoading]);
 
   return (
     <CharacterAnimationsContext.Provider
@@ -50,6 +63,10 @@ export const CharacterAnimationsProvider = (props) => {
         setAnimations,
         isSpeaking,
         setIsSpeaking,
+        isListening,
+        setIsListening,
+        isLoading,
+        setIsLoading,
       }}
     >
       {props.children}
